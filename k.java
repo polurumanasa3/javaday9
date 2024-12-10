5d5c728db59b1a73eb604957d4a835ab7526dd58
polurumanasa3 <manasap142@gmail.com>
2024-12-10 00:37:02 -0800

    Add files via upload

---
+++ k.java
@@ -0,0 +1,12 @@
+import java.util.*; 
+class k
+{
+    public static void main(String args[])
+    {
+    Scanner sc=new Scanner(System.in);
+    char s=sc.next().charAt(0);
+    s^=32;
+    System.out.println(s);
+    }
+}
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    
---
+++ l.java
@@ -0,0 +1,13 @@
+import java.util.*; 
+class l
+{
+    public static void main(String args[])
+    {
+    Scanner sc=new Scanner(System.in);
+    char s=sc.next().charAt(0);
+    if((s&32)==0)
+    System.out.println("Upper case");
+    else
+    System.out.println("Lower case");
+    }
+}                                                                                                                                                                    
